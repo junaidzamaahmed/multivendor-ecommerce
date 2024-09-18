4b1f6beeb1d8c4038ee858c7c72da8d91509041a
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-09-18 12:57:48 +0600

    single_product_page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'uoffice.com.sg',
+        pathname: '/**',
+          },
+        {
+        protocol: 'https',
+        hostname: 'spacemy.com.my',
+        pathname: '/cdn/shop/products/**',
+          },
+        {
+        protocol: 'https',
+        hostname: 'www.ikea.com',
+        pathname: '/**',
+          },
+        {
+        protocol: 'https',
+        hostname: 'i5.walmartimages.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
+// next.config.mjs
 export default nextConfig;
