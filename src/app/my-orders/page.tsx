915bf302de32f619265f01d1b54a9651a696d301
junaidzamaahmed <junaidahmedzama@gmail.com>
2024-09-23 06:28:33 +0600

    commited everything for now v3

--- src/app/my-orders/page.tsx
+++ src/app/my-orders/page.tsx
@@ -181,7 +181,7 @@ export default function CustomerDashboard() {
               <DialogHeader>
                 <DialogTitle>Edit Profile</DialogTitle>
                 <DialogDescription>
-                  Make changes to your profile here. Click save when you're
+                  Make changes to your profile here. Click save when you&apos;re
                   done.
                 </DialogDescription>
               </DialogHeader>
